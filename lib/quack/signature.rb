e5d12457998d7b2ed047bc6cf2759ff3166207ee
Emric <w.e.w@live.se>
2018-07-22 22:34:15 +0200

    Document the signature class

--- lib/quack/signature.rb
+++ lib/quack/signature.rb
@@ -1,10 +1,26 @@
+# Internal: The class responsible for massaging parameters before passing them
+# to the signatured method.
 class Quack::Signature
   attr_reader :signature
 
+  # Internal: Initialize a signature object.
+  #
+  # signature - The Symbol or Hash conversion method(s) that depicts this
+  #   signature.
   def initialize(*signature)
     @signature = signature
   end
 
+  # Internal: Convert the given arguments, with the given conversion methods.
+  #
+  # Examples:
+  #
+  #   Quack::Signature.new(:to_i, bignum: :to_s).apply!('1', 100)
+  #   #=> [1, { bignum: '100' }]
+  #
+  # Returns an Array of arguments.
+  # Raises Quack::SignatureError if the arguments could not be converted.
+  # Raises Quack::WrongNumberOfArgumentsError if wrong number of arguments.
   def apply!(*args)
     validate args, signature
     signature.each_with_index.map do |arg_signature, i|
@@ -14,10 +30,18 @@ class Quack::Signature
 
   private
 
+  # Internal: Returns a convertor to perform the given conversion.
+  #
+  # conversion - An Hash with conversions or a Symbol conversion.
+  #
+  # Returns a Quack::HashConvertor or Quack::DefaultConvertor instance.
   def convertor_for(conversion)
     convertor_class(conversion).new(conversion)
   end
 
+  # Internal: Returns the class to be used for applying the given conversion.
+  #
+  # conversion = An Hash with conversions or a Symbol conversion.
   def convertor_class(conversion)
     case conversion
     when Hash
@@ -27,10 +51,22 @@ class Quack::Signature
     end
   end
 
+  # Internal: Converts the given arguments with the given conversion(s).
+  #
+  #
+  # arguments - The Hash symbol arguments or any Object to convert.
+  # conversion = An Hash with conversions or a Symbol conversion.
+  #
+  # Returns the converted argument.
   def convert!(arguments, conversion)
     convertor_for(conversion).convert!(arguments)
   end
 
+  # Internal: Determine if correct number of arguments were given.
+  #
+  # Returns true if correct number of arguments.
+  # Raises Quack::WrongNumberOfArgumentsError if wrong number of arguments
+  #   given.
   def validate(arguments, signature)
     return true if arguments.length == signature.length
     raise Quack::WrongNumberOfArgumentsError
