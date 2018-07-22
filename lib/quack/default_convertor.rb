350a2add16ad3ba800b3e9d7d86ed89e8dfa48e1
Emric <w.e.w@live.se>
2018-07-22 22:15:19 +0200

    Document convertors

--- lib/quack/default_convertor.rb
+++ lib/quack/default_convertor.rb
@@ -1,12 +1,34 @@
+# Internal: The default convertor class to be used in order to convert a single
+# argument.
 class Quack::DefaultConvertor
   attr_reader :conversion_method
 
+  # Public: Initialize a DefaultConvertor.
+  #
+  # conversion_method - The method to call that does the conversion
   def initialize(conversion_method)
     @conversion_method = conversion_method
   end
 
+  # Public: Converts the given argument with the conversion method given in the
+  # initializer.
+  #
+  # argument - Any object to convert.
+  #
+  # Returns the converted argument.
+  # Raises Quack::SignatureError if the argument could not be converted.
   def convert!(argument)
-    return argument.public_send(conversion_method) if argument.respond_to? conversion_method
-    raise(Quack::SignatureError, "`#{argument}` must respond to `#{conversion_method}`")
+    return argument.public_send(conversion_method) if convertable?(argument)
+    raise(Quack::SignatureError,
+          "`#{argument}` must respond to `#{conversion_method}`")
+  end
+
+  private
+
+  # Internal: Tells whether the provided argument can be converted or not.
+  #
+  # Returns a Bool.
+  def convertable?(argument)
+    argument.respond_to? conversion_method
   end
 end
--- lib/quack/hash_convertor.rb
+++ lib/quack/hash_convertor.rb
@@ -1,12 +1,31 @@
+# Internal: The convertor class to iterate and convert symbol arguments or
+# hashes.
 class Quack::HashConvertor
   attr_reader :conversion_methods
 
+  # Public: Initialize a HashConvertor.
+  #
+  # conversion_methods - The keywords and conversion methods to be used.
   def initialize(conversion_methods)
     @conversion_methods = conversion_methods
   end
 
+  # Public: Converts the given symbol arguments with the provided conversion
+  # methods.
+  #
+  # argument_hash - The hash with arguments to convert.
+  #
+  # Examples:
+  #
+  #   convertor = Quack::HashConvertor.new(word: :to_s, number: :to_i)
+  #   convertor.convert!(word: nil, number: "100")
+  #   #=> { word: "", number: 100 }
+  #
+  # Returns an Hash with the converted arguments.
+  # Raises Quack::SignatureError if the arguments could not be converted.
   def convert!(argument_hash)
-    conversion_methods.each_with_object(argument_hash) do |(name, conversion), args|
+      conversion_methods
+        .each_with_object(argument_hash) do |(name, conversion), args|
       args[name] = Quack::DefaultConvertor.new(conversion).convert!(args[name])
     end
   end
